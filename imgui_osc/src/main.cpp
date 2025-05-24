7163e190b6530b92db1b0067a0078b26cbc8a26a
Alex Nivelle <alnivelle@gmail.com>
2025-05-23 19:21:57 -0700

    Realized my op amps were hooked up to power backwards (oops!) but now that they're fixed the response of the sensor is as advertised. Another change was making the average a float, which has fixed the issue with repeatability. I think I can safely increase the window size now to fix the noise, but also the noise may be insignificant when considering the end accuracy of the scale. Physically, I have replaced the feedback resistor with a pot to see if I can estimate a suitable value for the accuracy I want. If this is good enough I might not even need all 4 sensors

--- imgui_osc/src/main.cpp
+++ imgui_osc/src/main.cpp
@@ -4,8 +4,13 @@
 #include "imgui_impl_sdlrenderer3.h"
 #include <SDL3/SDL.h>
 #include <cstdio>
-#include <sys/socket.h>
-#include <array>
+#include <fstream>
+#include <deque>
+#include <glob.h>
+
+#define MAX_LINE_SIZE 15
+#define MAX_VECTOR_SIZE 300
+#define SERIAL_PATTERN "/dev/ttyACM*"
 
 int main(int argc, char *argv[]) {
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
@@ -13,6 +18,15 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    glob_t globs;
+    globs.gl_pathc = 0;
+  
+    // Get the serial port
+    while (globs.gl_pathc == 0) {
+        glob(SERIAL_PATTERN, 0, NULL, &globs);
+    }
+    const char *SERIAL = globs.gl_pathv[0];
+    printf("Found %s\n", SERIAL);
     // Create window with SDL_Renderer graphics context
     Uint32 window_flags = SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN;
     SDL_Window *window = SDL_CreateWindow("ImGuiOsc", 1280, 720, window_flags);
@@ -21,10 +35,12 @@ int main(int argc, char *argv[]) {
         SDL_Log("Error: SDL_CreateRenderer(): %s\n", SDL_GetError());
         return -1;
     }
+
     SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED,
                           SDL_WINDOWPOS_CENTERED);
     SDL_ShowWindow(window);
 
+    std::ifstream serial = std::ifstream(SERIAL);
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
@@ -45,16 +61,17 @@ int main(int argc, char *argv[]) {
 
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     bool done = false;
-    float test;
     ImGuiViewport *view = ImGui::GetMainViewport();
     ImGui::SetNextWindowSize(view->WorkSize);
     ImGui::SetNextWindowPos(view->WorkPos);
     ImGui::SetNextWindowViewport(view->ID);
-    std::array<float, 100> readings;
-    for (size_t i = 0; i < readings.max_size(); i++) {
-        readings[i] = 3000 * (float)rand() / RAND_MAX;
-    }
+
+    std::deque<float> readings(MAX_VECTOR_SIZE);
+    char dataline[MAX_LINE_SIZE];
+    int data;
+    float readData[MAX_VECTOR_SIZE];
     while (!done) {
+        // SDL Event stuff
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             ImGui_ImplSDL3_ProcessEvent(&event);
@@ -69,6 +86,19 @@ int main(int argc, char *argv[]) {
         ImGui_ImplSDL3_NewFrame();
         ImGui::NewFrame();
 
+        // Read data from the serial port
+        if (serial.is_open()) {
+            serial.getline(dataline, MAX_LINE_SIZE);
+            sscanf(dataline, "%d\n", &data);
+            if (readings.size() == MAX_VECTOR_SIZE) {
+                readings.pop_front();
+            }
+            readings.push_back((float)data);
+        } else
+            serial.open(SERIAL);
+        for (size_t i = 0; i < readings.size(); i++) {
+            readData[i] = readings[i];
+        }
         ImGuiWindowFlags window_flags =
             ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoResize;
         ImGui::SetNextWindowSize(view->WorkSize);
@@ -77,8 +107,8 @@ int main(int argc, char *argv[]) {
         ImGui::Begin("Oscope", NULL, window_flags);
         ImVec2 region = ImGui::GetContentRegionAvail();
         ImGui::PushItemWidth(region.x);
-        ImGui::PlotLines("##", readings.data(), readings.size(), 0, NULL,
-                         FLT_MIN, FLT_MAX, ImVec2(0, region.y / 2));
+        ImGui::PlotLines("##", readData, readings.size(), 0, NULL, 0, 4500,
+                         ImVec2(0, region.y / 2));
         ImGui::PopItemWidth();
         // ImGui::PlotLines();
         ImGui::End();
@@ -89,6 +119,7 @@ int main(int argc, char *argv[]) {
         ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
         SDL_RenderPresent(renderer);
     }
+    serial.close();
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
--- pico_code/src/main.c
+++ pico_code/src/main.c
@@ -3,13 +3,15 @@
 #include "hardware/adc.h"
 #include "pico/stdlib.h"
 
-#define AVERAGE_LEN 30
+#define AVERAGE_LEN 15
+#define ADC_PIN 26
+#define PWM_PIN 27
 
 int main() {
   stdio_init_all();
   adc_init();
   // printf("ADC measurement!\n");
-  adc_gpio_init(26);
+  adc_gpio_init(ADC_PIN);
 
   adc_select_input(0);
   adc_fifo_setup(true, false, 2, true, false);
@@ -23,7 +25,7 @@ int main() {
   adc_run(true);
   bool led_on = true;
 
-  uint32_t prev_avg = 0;
+  float prev_avg = 0;
   uint8_t index = 0;
   uint16_t window[AVERAGE_LEN];
   uint16_t result;
@@ -39,17 +41,16 @@ int main() {
   prev_avg /= AVERAGE_LEN;
 
   while(1) {
-    led_on = !led_on;
     result = adc_fifo_get();
     uint16_t last = window[index];
     
-    prev_avg += (result - last) / AVERAGE_LEN;
+    prev_avg += (float)(result - last) / AVERAGE_LEN;
     window[index] = result;
     index = (index + 1) % AVERAGE_LEN;
-    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
     if (!(result & (1 << 15)))
-      printf("%d\n", prev_avg);
-    sleep_ms(20);
+      printf("%d\n", (int)prev_avg);
+    
+    sleep_ms(10);
   }
   return 0;
 }
